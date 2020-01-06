eff0d8d4c870ade1451a477750c888129725ee39
Kailash Sethuraman <hsaliak@gmail.com>
2020-01-06 15:49:28 -0500

    added day8

--- day8.py
+++ day8.py
@@ -8,7 +8,7 @@ def pixels_from_file(fname : str) -> List[int]:
     vals = [int(v) for v in line]
     return vals
 
-def to_pixels(l : List[int], width=25, height=6) -> np.ndarray:
+def to_pixels(l : List[int], width: int =25, height: int =6) -> np.ndarray:
     ar = np.array(l)
     ar.shape = ( len(ar) // (width * height), width* height)
     return ar
@@ -21,6 +21,18 @@ def ones_and_twos_count(a : np.ndarray) -> int:
 def zero_count(a : np.ndarray) -> int:
     return sum(a == 0)
 
+def first_value(ars :  np.ndarray) -> None:
+    colcount = ars.shape[1]
+    image = np.ndarray(150,dtype=int)
+    for c in range(colcount):  # every element, every row, first column
+        pcol = ars[:,c]
+        image[c] = pcol[pcol != 2][0]
+    image.shape = (6, 25)
+    for i in range(0, 25, 5):
+        print(image[:, i:i+5])
+        print('\n\n')
+
+
 if __name__ == '__main__':
     vals = pixels_from_file("day8input")
     v = to_pixels(vals)
@@ -32,3 +44,4 @@ if __name__ == '__main__':
             zeros = rzero
             zidx =  idx
     print(ones_and_twos_count(v[zidx]))
+    first_value(v)
