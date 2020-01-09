ed3836354210d7f71e53b21140c25b2ba60c05fe
Kailash Sethuraman <hsaliak@gmail.com>
2020-01-09 17:51:44 -0500

    day 10.2

--- day10.py
+++ day10.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass,  field
+from operator import attrgetter
 from typing import List, Set
 import math
 
@@ -10,12 +11,29 @@ class Point:
     x : float
     y : float
 
+# unused
 def unit_vector(p1 : Point, p2 : Point) -> Point:
     direction = Point(p2.x - p1.x, p2.y - p1.y)
     distance = math.sqrt(direction.x ** 2 + direction.y **2 )
     unit : Point = Point(round(direction.x / distance, 4), round((direction.y / distance), 4))
     return unit
 
+
+@dataclass(eq = True, frozen = True, order = True)
+class Target:
+    angle : float  
+    magnitude : float   
+    def to_point(self, origin : Point) -> Point:
+        xmag  = round(math.sin(self.angle) * self.magnitude, 4)
+        ymag = round(math.cos(self.angle) * self.magnitude, 4)
+        return Point(origin.x + xmag, origin.y + ymag)
+
+def target_from_point(dest : Point,  origin : Point = Point(0,0)) -> Target:
+    dx = dest.x - origin.x 
+    dy = dest.y - origin.y 
+    magnitude = math.sqrt(dx **2 + dy ** 2)
+    angle =  math.atan2(dx, dy) # angle from y axis
+    return Target(angle,magnitude)
 
 @dataclass
 class Map:
@@ -34,21 +52,38 @@ class Map:
             mapstr += '\n'
         return mapstr
 
-    def visibles(self, p : Point) -> Set[Point]:
+    def visibles(self, p : Point, ignore : Set[Point]) -> Set[Target]:
+        if p not in ignore:
+            ignore.add(p)
         if p not in self.asteroids:
             raise ValueError(f"{p} not found in {self.asteroids}")
-        
         others = set(self.asteroids)
-        others.discard(p)
-        units : Set[Point] = set()
-        for a in others:
-            units.add(unit_vector(a, p))
+        others.difference_update(ignore)
+        targets : List[Target] = sorted((target_from_point(a, p) 
+                for a in others)) 
+        visible_asteroids : Set[Target] = set()
+        angles : Set[float] = set()
+        for t in targets:
+            if t.angle in angles:
+                continue
+            else:
+                angles.add(t.angle)
+                visible_asteroids.add(t)
+        return visible_asteroids
 
-        #print(f"{p}:  {len(units)}")
-        return units
+    def sweep(self, p : Point) -> List[Target]:
+        targets : List[Target] = list()
+        ignore : Set[Point] = {p}
+        sweep_targets = self.visibles(p, ignore=ignore)
+        while sweep_targets:
+            targets = targets + sorted(sweep_targets, key=attrgetter('angle'), reverse=True)
+            ignore = set.union(ignore, (t.to_point(p) for t  in sweep_targets))
+            sweep_targets = self.visibles(p, ignore)
+        return targets
+        
 
     def viscount(self, p : Point) -> int:
-        return len(self.visibles(p))
+        return len(self.visibles(p, set()))
 
     def max_visibles(self)->int:
         return max(self.viscount(a) for a in self.asteroids)
@@ -57,15 +92,12 @@ class Map:
         vcount : int = 0
         p : Point
         for a in self.asteroids:
-            visibles : Set[Point] = self.visibles(a)
-            if len(visibles) > vcount:
-                vcount = len(visibles)
+            vc = self.viscount(a) # how many
+            if vc > vcount:
+                vcount = vc  
                 p = a
-        return a      
+        return p      
 
-def angle(p :Point):
-    return math.atan2(p.y, p.x) # we want to measure against y axis
-            
 
 def read_map(filename : str) -> Map:
     with open(filename) as f:
@@ -80,12 +112,10 @@ def read_map(filename : str) -> Map:
     return Map(xlen, ylen, set(asteroids))
 
 
-m : Map = read_map("day10test")
-#print(m)
-#print(m.visibles(Point(x=1, y = 0)))
-print(m.max_visibles())
+m : Map = read_map("day10input")
 station = m.station_location()
-
-vectors =  sorted((a for a in m.visibles(station)), key = angle)
-print(vectors)
-
+print(station)
+print(m.viscount(station)) # part 1
+s = m.sweep(station)
+p = s[199].to_point(station)
+print(p.x* 100 +  p.y) # part 2
--- test_day10.py
+++ test_day10.py
@@ -13,8 +13,25 @@ def test_points():
     #assert len(s) == 1
 
 
+def test_target():
+    p = Point(2,3)
+    p2 = Point(4,5)
+    t = target_from_point(p2, p)
+    p2prime  = t.to_point(p)
+    assert p2 == p2prime
+    t1 = target_from_point(Point(2,3))
+    t3 = target_from_point(Point(4,6))
+    t4 = target_from_point(Point(1,0)) # 0 angle actually
+    t5 = target_from_point(Point(0,100))
+    t6 = target_from_point(Point(0,-0.01))
+    assert t3 > t1  # grater  magnitude
+    assert t4 > t1 #  this is a 90 degree
+    assert  t6>  t3 > t1 >  t5
+    print(sorted([t1,t3,t4,t5,t6]))
+
 def test_map():
     m  = read_map("day10test")
     print(m)
     assert m == Map(xlen=5, ylen=5, asteroids={Point(x=1, y=0), Point(x=4, y=0), Point(x=0, y=2), Point(x=1, y=2), Point(x=2, y=2), Point(x=3, y=2), Point(x=4, y=2), Point(x=4, y=3), Point(x=3, y=4), Point(x=4, y=4)})
     
+test_target()
