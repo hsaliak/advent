36f9ef1d532f9bbe976103d3235f68c4677f2b55
hsaliak <hsaliak@gmail.com>
2020-02-04 08:57:01 -0500

    .

--- day14.py
+++ day14.py
@@ -1,9 +1,10 @@
 from __future__ import annotations # to type OrderedDict
-from typing import Dict
+from typing import Dict, Optional
 from collections import deque, OrderedDict
 from typing import Deque, List , Union
 from dataclasses import dataclass
 from math import ceil
+from itertools import permutations
 
 
 
@@ -48,6 +49,7 @@ def isbase(s : str, chembook : Dict[str, Equation]) -> bool:
 
 def orecount(ones : Dict[str, int], chembook : Dict[str, Equation])-> int:
     amt : int = 0
+    print(f"ores from {ones}")
     assert len(ones) == len(set(ones.keys()))
     for chemical, count  in ones.items():
         base_chemicals = reaction(Chem(count, chemical), chembook )
@@ -57,28 +59,69 @@ def orecount(ones : Dict[str, int], chembook : Dict[str, Equation])-> int:
     print(amt)
     return amt 
     
-    
+def isreagent(c: str, e : Equation) -> bool:
+    reagents = set(x.elt for x in e.lhs)
+    if c in reagents:
+        return True
+    return False
+
+def transforms(frm : str, chembook: Dict[str, Equation] ) -> Optional[str]:
+    if isbase(frm, chembook):
+        return None
+        
+    for eq in chembook.values():
+        if len(eq.lhs) == 1:
+            if eq.lhs[0].elt == frm:
+                return eq.rhs.elt
+    return None
+
+def compactEquation(es : List[Chem] , chembook : Dict[str, Equation]) -> List[Chem]:
+    ce = es[:]
+    cni =  { v.elt : i for i , v in enumerate(es)}
+    elts = cni.keys()
+    for c in elts:
+        t = transforms(c, chembook) # t is what it transforms to 
+        if t and t in cni: # so we can compact
+            tidx = cni[t]
+            cidx = cni[c]
+            elt2c = reaction(es[tidx], chembook)[0] # this is the transforms to 
+            ce[cidx].count += elt2c.count
+            ce.pop(tidx)
+            cni =  { v.elt : i for i , v in enumerate(ce)} # reset dict
+            
+    return ce # no early return
+
+
+def addto(d : OrderedDict[str, int], k : str, v : int) -> None:
+    if k in d:
+        d[k] += v
+    else:
+        d[k] = v
+
 def chain_reaction2( c : Chem, chembook : Dict[str, Equation] = {} ) -> int:
     """ does a chain reaction and returns the amount of ore required """ 
     chemicals : OrderedDict[str, int] = OrderedDict(((e.elt, e.count) for e in reaction(c, chembook)))
-    #oneoffs : Dict[str, int]  = {}
     while any(not isbase(x, chembook) for x in chemicals.keys()): 
         print(f" chemicals {chemicals} \n ")
         cname, counts = chemicals.popitem(last=False) # pop the first thing out 
         if isbase(cname, chembook ): # leave it alone
             chemicals[cname] = counts # reinsert
             continue
-            #if cname in oneoffs:
-            #    oneoffs[cname] += counts
-            #else:
-            #    oneoffs[cname] = counts
-        else:
+        else: # it's not a base chemicals, do a reaction
+            
             base_chemicals = reaction(Chem(counts, cname), chembook) # get the reaction 
+            print(base_chemicals)
+            base_chemicals = compactEquation(base_chemicals, chembook )
+            print(base_chemicals)
+            
             for r in base_chemicals:
-                if r.elt in chemicals: # 
-                    chemicals[r.elt] += r.count
-                else:
-                    chemicals[r.elt] = r.count # insert into the ordered dictionary 
+                #t : Optional[str] = transforms(r.elt, chembook) # we can break this down
+                #if t:
+                #    tchems = reaction(r, chembook)
+                #    for tc in tchems:
+                #        addto(chemicals, tc.elt, tc.count)
+                #else:
+                addto(chemicals, r.elt, r.count) # add to catalog
     # now its all base chemicals
     return orecount(chemicals, chembook)
 
