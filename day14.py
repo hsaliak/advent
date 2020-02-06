c3e9200a62f1edd234f58958277147d07acb244c
hsaliak <hsaliak@gmail.com>
2020-02-06 00:27:21 -0500

    rewrite code for 14.1

--- day14.py
+++ day14.py
@@ -2,19 +2,30 @@ from __future__ import annotations # to type OrderedDict
 from typing import Dict, Optional
 from collections import deque, OrderedDict
 from typing import Deque, List , Union
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from math import ceil
 from itertools import permutations
+from heapq import heappush
+import operator
 
 
 
-base : str = 'ORE'
 
-
-@dataclass
+@dataclass(order=True, eq = True)
 class Chem:
-    count : int 
     elt : str
+    count : int 
+    weight : Optional[int]  = None
+
+    def get_weight(self, book : Dict[str, Equation]):
+        if not self.weight:
+            eq  : Equation = book[self.elt]
+            if eq.isbase:
+                self.weight = 1
+            else:
+                self.weight = sum(c.get_weight(book) for c in eq.lhs)  + 1
+        return self.weight
+
 
 @dataclass
 class Equation:
@@ -36,10 +47,10 @@ def reaction( c : Chem, chembook : Dict[str, Equation] ) -> List[Chem]:
     equation : Equation  = chembook[elt] # rhs is the 2A or 5A or similar, i want 10A
     nreactions : int  = 0
     nreactions =  ceil(num/equation.rhs.count )  # 10 / 2 is 5 so i run the equation 5 times
-    chems =  [Chem(c.count * nreactions, c.elt) for c in equation.lhs]  # c
+    chems =  [Chem(c.elt, c.count * nreactions) for c in equation.lhs]  # c
+    if not equation.isbase:
+        chems.sort(key=operator.methodcaller('get_weight', chembook))
     return chems
-
-
 
 def isore(c : Chem) -> bool:
     return c.elt == "ORE"
@@ -49,94 +60,38 @@ def isbase(s : str, chembook : Dict[str, Equation]) -> bool:
 
 def orecount(ones : Dict[str, int], chembook : Dict[str, Equation])-> int:
     amt : int = 0
-    #print(f"ores from {ones}")
     assert len(ones) == len(set(ones.keys()))
     for chemical, count  in ones.items():
-        base_chemicals = reaction(Chem(count, chemical), chembook )
+        base_chemicals = reaction(Chem(chemical, count), chembook )
         if  len(base_chemicals) != 1 or not isore(base_chemicals[0]):
             raise ValueError(f"{chemical} cannot be derived from ore")
         amt += ceil(base_chemicals[0].count)
-    print(amt)
     return amt 
-    
-def isreagent(c: str, e : Equation) -> bool:
-    reagents = set(x.elt for x in e.lhs)
-    if c in reagents:
-        return True
-    return False
-
-def transforms(frm : str, chembook: Dict[str, Equation] ) -> Optional[str]:
-    for eq in chembook.values():
-        if len(eq.lhs) == 1:
-            if eq.lhs[0].elt == frm:
-                return eq.rhs.elt
-    return None
-
-def decompose(to : str, chembook : Dict[str, Equation]) -> Optional[str]:
-    eq = chembook[to]
-    if len(eq.lhs) == 1 and eq.lhs[0].elt != "ORE":
-        return eq.lhs[0].elt
-    return None
-
-def compactEquation(es : List[Chem] , chembook : Dict[str, Equation]) -> List[Chem]:
-    # for each item in the equation, check if it can be transformed into another present
-    # if it can be, then remove it from the list and add it. 
-    # maybe do an ordered Dict of Key and Value
-    esd : OrderedDict[str, int]  = OrderedDict()
-    for x in es:
-        if x.elt in esd:
-            esd[x.elt] += x.count
-        else:
-            esd[x.elt] = x.count
-    esdi = esd.copy()
-    for e in esdi.keys():
-        if e == "ORE":
-            print("here is some ore ")
-            continue
-        t = decompose(e, chembook)
-        if t and t in esd: # so we can compact
-            print(f"compacting {es} by decomposing {e} to  {t}")
-            reagent = reaction(Chem(esd[e], e), chembook)[0] 
-            print(reagent.elt, e, ":", t)
-            esd[reagent.elt] += reagent.count # bmbpt
-            esd.pop(e) # get rid of it  -- this now has bmbt only
-            print(esd)
-            return compactEquation([Chem(v,k) for k,v in esd.items()], chembook)
-            
-    return [Chem(v,k) for k,v in esd.items()] 
-
 
 def addto(chemicals : List[Chem], c : Chem) -> None:
-    if c in chemicals:
-        chemicals[chemicals.index(c)].count += c.count
+    eltidx = {x.elt: idx for idx, x in enumerate(chemicals)}
+    if c.elt in eltidx:
+        chemicals[eltidx[c.elt]].count += c.count
     else:
         chemicals.append(c)
 
 def chain_reaction2( c : Chem, chembook : Dict[str, Equation] = {} ) -> int:
     """ does a chain reaction and returns the amount of ore required """ 
     chemicals = reaction(c, chembook)
-    chemicals = compactEquation(chemicals, chembook) # compact the equation
     basechems : Dict[str, int] =  {}
-    oreamt = 0
-    while  chemicals: 
+    while chemicals: 
         c = chemicals.pop()
-        if isore(c): # leave it alone
-            oreamt += c.count
-        elif isbase(c.elt, chembook):
+        if isbase(c.elt, chembook):
             if c.elt not in basechems:
-                basechems[c.elt] = c.count
+               basechems[c.elt] = c.count
             else:
                 basechems[c.elt] += c.count
         else: # it's not a base chemicals, do a reaction
-            base_chemicals = reaction(c , chembook) 
-            base_chemicals = compactEquation(base_chemicals, chembook )
+            base_chemicals = reaction(c , chembook)  # this is sorted now
             for r in base_chemicals:
-                if isore(r):
-                    oreamt += r.count
-                else:
-                    chemicals.append(r)
-                    chemicals = compactEquation(chemicals, chembook)
-    return oreamt + orecount(basechems, chembook)
+                addto(chemicals, r)
+            chemicals.sort(key=operator.methodcaller('get_weight', chembook))
+    return  orecount(basechems, chembook)
 
 def parse_chembook(fname : str) -> Dict[str, Equation]:
     with open(fname) as f:
@@ -145,20 +100,20 @@ def parse_chembook(fname : str) -> Dict[str, Equation]:
     for l in lines:
         lhsstr, rhsstr = l.split("=>")
         rhscounts, rhschem = rhsstr.strip().split(" ")
-        rhs = Chem(int(rhscounts), rhschem)
+        rhs = Chem(rhschem, int(rhscounts))
         lhs = []
         reagents = lhsstr.split(",")
         for p in reagents:
             count, cname = p.strip().split(" ")
-            chem = Chem(int(count), cname)
+            chem = Chem(cname, int(count))
             lhs.append(chem)
         # now we are ready to append to the dictionary
         chembook[rhs.elt] = Equation(lhs, rhs)
     return chembook
 
+
     
 if __name__ == '__main__':
-    fuel : Chem = Chem(1, "FUEL")
-    chembook = parse_chembook("testinput5")
-    amt = chain_reaction2(fuel, chembook)
-    print(f"final {amt}")
+    chembook = parse_chembook("day14input")
+    c = Chem("FUEL", 1)
+    print(chain_reaction2(c, chembook))
--- test_day14.py
+++ test_day14.py
@@ -7,15 +7,14 @@ def test_day14inputs():
     cbook3 = parse_chembook("testinput3")
     cbook4 = parse_chembook("testinput4")
     cbook5 = parse_chembook("testinput5")
-    fuel = Chem(1, "FUEL")
-    #chembook = cbook1
-    #assert chain_reaction2(fuel, cbook1) == 31
-    #chembook = cbook2
-    #assert chain_reaction2(fuel, cbook2) == 165
-    #chembook = cbook3
-    #assert chain_reaction2(fuel, cbook3) == 13312
-    #chembook = cbook4
-    #assert chain_reaction2(fuel, cbook4) == 180697
+    fuel = Chem( "FUEL", 1)
+    assert chain_reaction2(fuel, cbook1) == 31
+    chembook = cbook2
+    assert chain_reaction2(fuel, cbook2) == 165
+    chembook = cbook3
+    assert chain_reaction2(fuel, cbook3) == 13312
+    chembook = cbook4
+    assert chain_reaction2(fuel, cbook4) == 180697
     chembook = cbook5
     assert chain_reaction2(fuel, cbook5) == 2210736
 
